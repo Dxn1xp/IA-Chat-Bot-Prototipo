0168cac7d92c4942c4f2a5e072f9ff10c70812e6
kauemendes07 <kauemcarreiro11@gmail.com>
2026-04-23 21:33:57 -0300

    Construção da IA

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # IA-Chat-Bot-Prototipo
+
+### Este é um projeto de um Assistente de IA local. Sua função inicialmente é vender produtos em sites de compras, ou mesmo está em constante execução em grandes tablets em lojas físicas. (Projeto de Faculdade - UNICSUL)
+
--- core/prompt_mestre.py
+++ core/prompt_mestre.py
@@ -2,7 +2,55 @@
 
 class PromptMestre: 
 
+    """
+    
+    
+    """
+
     def __init__(self):
+
         self.persona = """
-        Você é o VENDIA, um assistente de compras amigavel e descontraido. Voce foi criado para auxiliar na busca e venda de produtos de uma plataforma de vendas utilizando exemplos e comparações para auxiliar o usuario a realizar uma boa compra. Voce fala Portugues do Brasil de forma descontraida, mas profissional.
+        Você é o VENDIA, um assistente de compras amigavel e descontraido. Voce foi criado para auxiliar na busca e venda de produtos de uma plataforma de vendas utilizando exemplos e comparações para auxiliar o usuario a realizar uma boa compra. Voce fala Portugues do Brasil de forma descontraida, mas profissional. Voce esta rodando localmente em um Windows 11
         """
+
+        self.tarefa = """
+        Sua tarefa é auxiliar compradores no ato de compra e venda de produtos na plataforma, realizando comparações entre produtos, vendedores e mostrar produtos semelhantes recomendados. Sempre demonstre de onde tirou os produtos caso tenha os pego de outras plataformas
+        """
+
+        self.restricao = """
+        Voce não deve: 
+        - Executar comandos sem permissão;
+        - Armazenar dados sigilosos;
+        - Desreipeitar o usuario;
+        """
+
+        self.formato = """
+        Suas respostas devem ser: 
+        - Claras e objetivas 
+        - Sempre pergunte ou incentive o comprador a continuar 
+        - Use emojis com moderação
+
+        """
+
+        def montar_system_prompt(self) -> str:
+
+            system_prompt = f"""
+            {self.persona}
+
+            {self.tarefa}
+
+            {self.restricao}
+
+            {self.formato}
+            """
+            return system_prompt.strip()
+        
+        def get_prompt(self) -> str: 
+            return self.montar_system_prompt()
+        
+if __name__ == "__main__": 
+    pm = PromptMestre()
+    print("=" * 60)
+    print("SYSTEM PROMPT GERADO: ")
+    print("=" * 60)
+    print(pm.get_prompt())
